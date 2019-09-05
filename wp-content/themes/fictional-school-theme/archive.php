3c3845be445ea8dbac6885681de1439b4eff2e49
spierre95 <stephen.pierre20@gmail.com>
2019-09-05 09:58:10 -0400

    added custom archive page for posts

--- wp-content/themes/fictional-school-theme/archive.php
+++ wp-content/themes/fictional-school-theme/archive.php
@@ -3,9 +3,20 @@
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ocean.jpg)"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Welcome to our Blog</h1>
+    <h1 class="page-banner__title">
+    <?php 
+        the_archive_title()
+        // if(is_category()) {
+        //     single_cat_title();
+        // }
+
+        // if(is_author()) {
+        //   echo "Posts by ";  the_author();
+        // }
+    ?>
+    </h1>
     <div class="page-banner__intro">
-      <p>Keep up with our latest news</p>
+      <p><?php the_archive_description()?></p>
     </div>
   </div>
 </div>
--- wp-content/themes/fictional-school-theme/functions.php
+++ wp-content/themes/fictional-school-theme/functions.php
@@ -1,17 +1,20 @@
-<?php 
+<?php
 
-function load_styles(){
+function load_styles()
+{
     wp_enqueue_style('google_fonts', "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     wp_enqueue_style('font_awesome', "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
     wp_enqueue_style('main_styles', get_stylesheet_uri());
 }
 
-function load_scripts() {
+function load_scripts()
+{
     //remove micro time in production, this is used to prevent caching of just this file. 
     wp_enqueue_script('main_js', get_theme_file_uri('/js/scripts-bundled.js'), null, microtime(), true);
 }
 
-function load_features() {
+function load_features()
+{
     //adds custom title for each page in word press;
     add_theme_support('title-tag');
 
@@ -24,6 +27,15 @@ function load_features() {
 }
 
 
+
 add_action('wp_enqueue_scripts', 'load_styles');
 add_action('wp_enqueue_scripts', 'load_scripts');
 add_action('after_setup_theme', 'load_features');
+
+// removes prefix from function output ( i.e. Author:) and adds custom title for author 
+add_filter('get_the_archive_title', function ($title) {
+    if (is_author()) {
+        $title = 'Posted By ' . get_the_author();
+    }
+    return preg_replace('/^\w+: /', '', $title);
+});
